da8d51fb63fd94b87a232e6f8b5bdba40c2b321d
Pacharaphon Plathong <pacharaphon2014@gmail.com>
2024-06-18 01:09:14 +0700

    collistion detection

--- game.js
+++ game.js
@@ -5,28 +5,28 @@ const fps=60;
 const root2 = Math.pow(2,0.5);
 
 let player = {
-    x: 100,
+    x: 20,
     y: 50,
-    width:10,
-    height:10,
+    radius:10,
     health:100,
     speedX:5,
     speedY:5,
 }
 
-let maze = [[1,1,1,0,1,1],
-    [1,1,0,1,1,1],
-    [0,0,0,0,1,1],
-    [1,0,1,0,0,1],
-    [1,0,1,1,0,1],
-    [1,1,1,1,0,1],];
+let maze = [[1,0,1,0,0,1,1],
+    [1,0,1,0,1,1,1],
+    [0,0,0,0,1,0,0],
+    [1,0,1,0,0,0,1],
+    [1,0,1,1,1,1,1],
+    [1,0,0,0,0,1,1],
+    [1,1,1,1,0,0,0]];
 
-let maze_column = 6;
-let maze_row = 6;
+let maze_column = 7;
+let maze_row = 7;
 let maze_width = 30;
 let maze_heigth = 30;
-let maze_x = 0;
-let maze_y = 0;
+let maze_x = 100;
+let maze_y = 50;
 let w_pressed = false;
 let a_pressed = false;
 let s_pressed = false;
@@ -65,44 +65,69 @@ function playerUpdate(){
 }
 
 function collisionDetectionX(e,dx) {
-    if(dx>0){
-        for(let i=0;i<maze_row;i++){
-            for(let j=0;j<maze_column;j++){
-                if(maze[i][j]==1){
-                    let left_check=maze_x+j*maze_width;
-                    if(left_check>e.x+e.width && left_check<e.x-e.width+dx && ((e.y<maze_y+i*maze_heigth && e.y+e.height>i*maze_heigth)||(e.y<maze_y+(i-1)*maze_heigth && e.y+e.height>(i-1)*maze_heigth))) {
-                        e.x = left_check-e.width;
-                        ctx.beginPath();
-                        ctx.rect(maze_x+j*maze_width,maze_y+i*maze_heigth, maze_width,maze_heigth);
-                        ctx.fillStyle = "blue";
-                        ctx.fill();
-                        ctx.closePath();
-                        return true;
-                    }
-                }
-            }
-        }
-    } else if(dx<0){
-        for(let i=0;i<maze_row;i++){
-            for(let j=0;j<maze_column;j++){
-                if(maze[i][j]==1){
-                    
-                    let right_check=maze_x+(j+1)*maze_width;
-                    if(right_check>e.x-e.width+dx && right_check<e.x+e.width && ((e.y<maze_y+i*maze_heigth && e.y+e.height>i*maze_heigth)||(e.y<maze_y+(i-1)*maze_heigth && e.y+e.height>(i-1)*maze_heigth))) {
-                        ctx.beginPath();
-                        ctx.rect(maze_x+j*maze_width,maze_y+i*maze_heigth, maze_width,maze_heigth);
-                        ctx.fillStyle = "blue";
-                        ctx.fill();
-                        ctx.closePath();
-                        e.x = right_check+e.width;
-                        return true;
+    if(dx==0) return true;
+    let top_e = e.y-e.radius;
+    let bot_e = e.y+e.radius;
+    let left_e = e.x-e.radius;
+    let right_e = e.x+e.radius;
+    for(let i=0;i<maze_row;i++){
+        for(let j=0;j<maze_column;j++){
+            if(maze[i][j]==1){//wall
+                let top_m = maze_y+i*maze_heigth;
+                let bot_m = maze_y+(i+1)*maze_heigth;
+                //check y
+                if((top_m>top_e && top_m<bot_e) || (bot_m>top_e && bot_m<bot_e) || (top_m<e.y && bot_m>e.y)){
+                    if(dx>0){
+                        let left_m = maze_x+(j)*maze_width;
+                        if(right_e<=left_m && right_e+dx>=left_m){
+                            e.x = left_m-e.radius;
+                            return true;
+                        }
+                    } else {
+                        let right_m = maze_x+(j+1)*maze_width;
+                        if(left_e>=right_m && left_e+dx<=right_m){
+                            e.x = right_m+e.radius;
+                            return true;
+                        }
                     }
                 }
             }
         }
     }
     return false;
-    
+}
+
+function collisionDetectionY(e,dy){
+    if(dy==0) return true;
+    let left_e = e.x-e.radius;
+    let right_e = e.x+e.radius;
+    let top_e = e.y-e.radius;
+    let bot_e = e.y+e.radius;
+    for(let i=0;i<maze_row;i++){
+        for(let j=0;j<maze_column;j++){
+            if(maze[i][j]==1){//wall
+                let left_m = maze_x+(j)*maze_width;
+                let right_m = maze_x+(j+1)*maze_width;
+                //check y
+                if((left_m>left_e && left_m<right_e) || (right_m>left_e && right_m<right_e) || (left_m<e.x && right_m>e.x)){
+                    if(dy>0){
+                        let top_m = maze_y+i*maze_heigth;
+                        if(bot_e<=top_m && bot_e+dy>=top_m){
+                            e.y = top_m-e.radius;
+                            return true;
+                        }
+                    } else {
+                        let bot_m = maze_y+(i+1)*maze_heigth;
+                        if(top_e>=bot_m && top_e+dy<=bot_m){
+                            e.y = bot_m+e.radius;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
 
 function draw(){
@@ -113,7 +138,7 @@ function draw(){
 
 function drawPlayer(){
     ctx.beginPath();
-    ctx.arc(player.x,player.y, player.width, 0, Math.PI * 2);
+    ctx.arc(player.x,player.y, player.radius, 0, Math.PI * 2);
     ctx.fillStyle = "yellow";
     ctx.fill();
     ctx.closePath();
@@ -134,16 +159,16 @@ function drawMaze(){
 }
 
 function move(e,dx,dy) {
-    if(e.x-e.width+dx<0 || e.x+e.width+dx>canvas.width) {
-        if(dx<0) e.x=e.width;
-        else e.x = canvas.width-e.width;
+    if(e.x-e.radius+dx<0 || e.x+e.radius+dx>canvas.width) {
+        if(dx<0) e.x=e.radius;
+        else e.x = canvas.width-e.radius;
         console.log("hit wall");
     } else if(!collisionDetectionX(e,dx)) e.x+=dx;
-    if(e.y-e.height+dy<0 || e.y+e.height+dy>canvas.height) {
-        if(dy<0) e.y=e.height;
-        else e.y = canvas.height-e.height;
+    if(e.y-e.radius+dy<0 || e.y+e.radius+dy>canvas.height) {
+        if(dy<0) e.y=e.radius;
+        else e.y = canvas.height-e.radius;
         console.log("hit wall");
-    } else e.y+=dy;
+    } else if(!collisionDetectionY(e,dy)) e.y+=dy;
 }
 
 $(".start-button").on("click", function() {
